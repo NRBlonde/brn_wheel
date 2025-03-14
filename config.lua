4e36b20fe6fa1b439e11a35abf23c95dcb993f56
NRBlonde <74438432+NRBlonde@users.noreply.github.com>
2025-03-14 18:50:31 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {
+    NonSlipperyVehicles = {"sultan", "adder", "banshee", "comet", "t20"}, -- etkilenmeyecek araçların listesi
+    CheckInterval = 4000 -- kaç saniyede bir olacağını belirler
+}

2546f360e597613ea26d9b7b53292cb656b0da1d
NRBlonde <74438432+NRBlonde@users.noreply.github.com>
2025-03-14 18:50:09 +0300

    Delete brn_wheel directory

--- brn_wheel/config.lua
+++
@@ -1,4 +0,0 @@
-Config = {
-    NonSlipperyVehicles = {"sultan", "adder", "banshee", "comet", "t20"}, -- etkilenmeyecek araçların listesi
-    CheckInterval = 4000 -- kaç saniyede bir olacağını belirler
-}

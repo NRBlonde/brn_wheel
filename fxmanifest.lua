a0caa20b35b827cc99eb6375d1438082a02d2ef5
NRBlonde <74438432+NRBlonde@users.noreply.github.com>
2025-03-14 18:53:54 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Baran909'
+description 'Araç kayma ve direksiyon kontrol scripti'
+version '1.0.0'
+
 client_scripts {
     'config.lua',
     'client.lua'
